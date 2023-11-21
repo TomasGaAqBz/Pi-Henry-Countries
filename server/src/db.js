1b70c6100d2d427c6f965d87b5ee32be50b273d5
Tomas Aquino <tomasaquinogabi@gmail.com>
2023-11-21 16:04:49 -0300

    Añadiendo imagenes para facilitar el proyecto y Terminando base y modelo de base de datos

--- server/src/db.js
+++ server/src/db.js
@@ -4,14 +4,15 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, 
   native: false, 
 });
 const basename = path.basename(__filename);
+sequelize.authenticate().then(()=>{console.log("FOFO FUnciona en la DB");}).catch((error)=>{console.log("tenemos un error: ", error);})
 
 const modelDefiners = [];
 
@@ -28,10 +29,12 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Country } = sequelize.models;
+const { Country , Activity} = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+//! Relacion Muchos a muchos a traves de Activity_Country
+Country.belongsToMany(Activity, { through: "Activity_Country" });
+Activity.belongsToMany(Country, { through: "Activity_Country" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -4,9 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+      allowNull: false,
+      validate:{
+        len:{
+          args:[3,3]
+        }
+      }
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    flagImage: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    coatOfArms:{
+      type: DataTypes.STRING,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false      
+    },
+    subregion: {
+      type: DataTypes.STRING,
+     
+    },
+    area: {type: DataTypes.FLOAT,
+      
+    },
+    population: {type: DataTypes.INTEGER,
+      allowNull:false,
     },
   });
 };
